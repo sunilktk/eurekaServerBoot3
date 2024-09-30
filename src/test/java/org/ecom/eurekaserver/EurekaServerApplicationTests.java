0f41bcb7ff51914a70fdb4eed308500bf6f75e4f
Sunil <sunil@test.com>
2024-09-29 21:28:46 -0400

    ecomm-eureka-server

---
+++ src/test/java/org/ecom/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecom.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class dEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
